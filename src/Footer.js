89dd3f1803740b9ddf96d9eef2e0db5d387224c2
Waldner <arvid.waldner@cgi.com>
2023-06-30 15:44:06 +0200

    new navbar

--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Container, Row, Col, Card } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCode, faTriangleExclamation, faPersonDigging } from "@fortawesome/free-solid-svg-icons";
+import { faCode, faTriangleExclamation, faPersonDigging} from "@fortawesome/free-solid-svg-icons";
 
 function Footer(){
 
@@ -15,20 +15,20 @@ function Footer(){
                             <span className="small"><h5 className="footerText"><FontAwesomeIcon icon={faCode} bounce /> Mythology Gallery - a React beginners project</h5></span> 
                         </Col>
                         <Col sm="4">
-                            
+                            <span className="small"><h5 className="footerText"><FontAwesomeIcon icon={faPersonDigging} bounce /> Work in progress since june 2023</h5></span>  
                         </Col>
                         <Col sm="4">
-                        
+                            <span className="small"><p className="footerText"><FontAwesomeIcon icon={faTriangleExclamation} bounce/> I do NOT own the images displayed and I take no credit for them.</p></span>
                         </Col>
                     </Row>
                     <Row>
                         <Col sm="6">                            
-                            <span className="small"><p className="footerText"><FontAwesomeIcon icon={faPersonDigging} bounce /> Work in progress since 2023</p></span>                            
+                                                       
                         </Col>
                         <Col sm="6">                           
-                            <span className="small"><p className="footerText"><FontAwesomeIcon icon={faTriangleExclamation} bounce/> I do NOT own the images displayed and I take no credit for them.</p></span>                        
+                                                    
                         </Col>                        
-                    </Row>
+                    </Row>                    
                 </Container>                    
             </Container>                        
         </>
--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import { Container, Navbar, Nav } from "react-bootstrap";
 import { GenIcon } from "react-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBolt, faWater, faSkull, faVenus, faShield, faChessQueen, faWineGlass} from "@fortawesome/free-solid-svg-icons";
+import { faBars, faSun} from "@fortawesome/free-solid-svg-icons";
 import { useActiveMenu, registerTriggerRef, handleTriggerClick } from "react-active-menu";
 import './styles/Navbar.css';
 
@@ -14,32 +14,47 @@ import './styles/Navbar.css';
 
 function NavbarComponent(){
 
-    
+    const [Open, setOpen] = useState(false);
+    const[sticky, setSticky]=useState();
+
+    useEffect(()=>{
+        const handleScroll=()=>setSticky(window.scrollY>200)
+        console.log(window.scrollY)
+        window.addEventListener('scroll', handleScroll)
+        return()=> window.removeEventListener('scroll', handleScroll)
+      })
 
     return(
         <>
-        
-            <header className="nav">
-                <nav className="nav__container__actions">
-                    <ul>
-                        <li>
-                            <Link activeClass="active" to="greek" spy={true} smooth={true} offset={-70} duration={500}>                   
-                                Greek mythology
-                            </Link>
-                        </li>
-                        <li>
-                            <Link activeClass="active" to="norse" spy={true} smooth={true} offset={-70} duration={500}>                   
-                                Norse mythology
-                            </Link>
-                        </li>
-                        <li>
-                            <Link activeClass="active" to="egyptian" spy={true} smooth={true} offset={-70} duration={500}>                   
-                                Egyptian mythology
-                            </Link>
-                        </li>			        			        
-                    </ul>
-                </nav>
-            </header>    
+        <nav className={`${sticky ? "sticky" : ""}`}>
+            <div className="Navbar">
+                <span className='nav-logo'>
+                    <h2>Mythology Gallery</h2>
+                </span>
+                <div className={`nav-items ${Open && "open"}`}>
+                    <a>
+                        <Link activeClass="active" to="greek" spy={true} smooth={true} offset={-70} duration={500}>                 
+                            Greek mythology
+                        </Link>
+                    </a>
+                    <a>
+                        <Link activeClass="active" to="norse" spy={true} smooth={true} offset={-70} duration={500}>                   
+                            Norse mythology
+                        </Link>    
+                    </a>
+                    <a>
+                        <Link activeClass="active" to="egyptian" spy={true} smooth={true} offset={-70} duration={500}>                   
+                            Egyptian mythology
+                        </Link>    
+                    </a>                    
+                </div>
+                <div className={`nav-toggle ${Open && "open"}`} onClick={() => setOpen(!Open)}>
+                    <div className="bar">
+
+                    </div>
+                </div>
+            </div>
+        </nav>        
                 
         </>
     
